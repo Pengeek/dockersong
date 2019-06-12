ad00145b90b224cf5eba495229add11988119a1e
Maciej <macciej.net@gmail.com>
2019-06-12 20:54:36 +0200

    init app

---
+++ db/migrate/20190611222223_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :author
+      t.float :time
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
